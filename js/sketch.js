5d76f9b86c34127946354c3804425f17527623cc
Chris Booth <31346759+chrisPhat@users.noreply.github.com>
2021-12-04 09:38:03 +0000

    Smashing Mag Background

--- js/sketch.js
+++ js/sketch.js
@@ -1,50 +1,47 @@
 var img;
 
-const snowFlakeCount = 500;
+const snowFlakeCount = 2000;
 const size = 5;
 const gravity = 0.5;
 
-var snowFlakes = [];
+const snowFlakes = [];
 
 function preload(){
-    img = loadImage('../img/bg.jpeg')
+    img = loadImage('img/bg.png');
 }
 
 function setup() {
-    var canvas = createCanvas(1280, 720);
+    var canvas = createCanvas(window.innerWidth, window.innerHeight);
     canvas.parent("p5container");
 
     
 
-    for(let i = 0; i < snowFlakeCount; i++){
-        
+    noStroke();
+  
+    rectMode(CENTER);
+    
+    for(let i = 0; i < snowFlakeCount; i ++){
         snowFlakes.push({
-            x: random(width),
-            y: random(height)
-        });
-
-        
+        x: random(width),
+        y: random(height)
+        })
     }
-
 }
 
 function draw() {
 
     image(img, 0, 0, width, height);
-   noStroke();
-   fill(255, 255, 255);
-
-   for(let i = 0; i < snowFlakes.length; i++){
-       var snowflake = snowFlakes[i];
-    
-       ellipse(snowflake.x, snowflake.y, size);
-
-       if(snowflake.y > height + size){
-        snowflake.y = -size;
-       } else {
-        snowflake.y += gravity;
-       }
-   }
-
-   
+    fill(255);
+  
+    for(let i = 0; i < snowFlakes.length; i++){
+      let snowFlake = snowFlakes[i];
+      
+      ellipse(snowFlake.x, snowFlake.y, size);
+      
+      if(snowFlake.y > height + size){
+       snowFlake.y = -size;
+      } else {
+        snowFlake.y += gravity
+      }
+    }
 }
